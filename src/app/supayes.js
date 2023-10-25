30dccfc779a4ff121add9450fdcf8bf5af3683e6
rishyendra333 <129914241+rishyendra333@users.noreply.github.com>
2023-10-25 17:14:58 -0400

    Update supayes.js

--- src/app/supayes.js
+++ src/app/supayes.js
@@ -2,14 +2,17 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient('https://yxxeykqqytazrtqgekup.supabase.co', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4eGV5a3FxeXRhenJ0cWdla3VwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODE2NDg3MiwiZXhwIjoyMDEzNzQwODcyfQ.d6NTLLcurXktkX1Su_uXU24nW-AwwZ2lmrqC2-ccvXQ");
 
-
-async function insertUsers(tid, tname, tpoints=0, tcontact=null, thelper=null, tcompleted_orders=null, tin_progress=null) {
-
+async function insertUser(id, name, points = 0, contact = null, helper = null, completed_orders = null, in_progress = null) {
+    if (id === undefined || name === undefined) {
+        console.error('Error: id and name are required parameters.');
+        return;
+    }
+    
     try {
         const { data, error } = await supabase
             .from('Users')
             .insert([
-                { id: tid, name: tname, points: tpoints, contact: tcontact, helper: thelper, completed_orders: tcompleted_orders, in_progress: tin_progress }
+                { id: id, name: name, points: points, contact: contact, helper: helper, completed_orders: completed_orders, in_progress: in_progress }
             ]);
 
         if (error) {
@@ -41,5 +44,5 @@ async function fetchUsers() {
     }
 }
 
-insertUser(5,tname='sub5', tcontact='5sub@gma',tcompleted_orders='5asdfjlks');
-fetchUser();
+insertUser(5, "tryingparamSub")
+fetchUsers();
